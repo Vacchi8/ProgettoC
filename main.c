13afe8a94d55e4b4c002c861b3e1230fcb773ff4
Vacchi8 <118276492+Vacchi8@users.noreply.github.com>
2023-04-30 09:38:25 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "functions.h"
+#include "struct.h"
+
+int main(int argc, char const *argv[])
+{   
+    int selett=0;
+do{
+    printf("Legenda azioni\n");
+    printf("\n");
+    printf("1)Creare classe\n");
+    printf("\n");
+    printf("2)Inserire un voto\n");
+    printf("\n");
+    printf("3)Cancellare un voto\n");
+    printf("\n");
+    printf("4)Visualizzare voti della classe\n");
+    printf("\n");
+    printf("5)Voti di una materia\n");
+    printf("\n");
+    printf("6)Voti di uno studente\n");
+    printf("\n");
+    printf("7)I voti piu alti\n");
+    printf("\n");
+    printf("[per uscire premere 0]\n");
+    printf("\n");
+    printf("Inserire un azione\n");
+    scanf(" %d", &selett);
+    switch(selett){
+    case 1:
+        printf("\n");
+        NewClass();
+        printf("\n");
+        break;
+    case 2:
+        printf("\n");
+        Insvoto();
+        printf("\n");
+        break;
+    case 3:
+        printf("\n");
+        Cancvoto();
+        printf("\n");
+        break;
+    case 4:
+        printf("\n");
+        VotoClasse();
+        printf("\n");
+        break;
+    case 5:
+        printf("\n");
+        VotoMat();
+        printf("\n");
+        break;
+    case 6:
+        printf("\n");
+        VotoStudente();
+        printf("\n");
+        break;
+    case 7:
+        printf("\n");
+        VotiMagg();
+        printf("\n");
+        break;
+    case 0:
+        break;
+    default:
+    printf("Numero inserito non consetito,riprovare\n");
+    printf("\n");
+    }
+
+}while(selett!=0);
+
+printf("Arresto del programma\n");
+return 0;
+}
